2ff6a5982858332dfa726286c760d02367d3fafa
Anmol Tharu Chaudhary <anmoldongriya123@gmail.com>
2025-08-05 15:31:59 +0545

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,35 @@
+// next.config.js
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'th.bing.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "th.bing.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'www.bing.com',
-        port: '',
-        pathname: '/**',
-      },{
+        protocol: "https",
+        hostname: "www.bing.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
         protocol: "https",
         hostname: "dribbble.com",
         port: "",
@@ -80,9 +83,9 @@ const nextConfig: NextConfig = {
         port:"",  
         pathname:"/**",
       },
-      
     ],
   },
 };
 
 export default nextConfig;
+  
